0c65ee8213d223c5d4469f37807942f04032f424
Gabriela Galarza <gabrielagalarza538@gmail.com>
2026-08-20 15:10:59 -0300

    Actualiza cambios de Gabriela

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
 import CursosPage from "./pages/Cursospage";
+import TestPage from "./pages/TestPage";
 
 function App() {
-  return <CursosPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/cursos" element={<CursosPage />} />
+        <Route path="/test" element={<TestPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
